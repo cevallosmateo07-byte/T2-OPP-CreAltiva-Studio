d590080a806ea0f272785d8e72189895126bfffd
cevallosmateo07-byte <cevallosmateo07@gmail.com>
2025-11-11 00:02:47 -0500

    code v0.2.0

---
+++ 06Code/ProyectoCrealtivaStudio/CrealtivaStudio/src/ec/edu/espe/crealtivastudio/controller/BillController.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.crealtivastudio.controller;
+import ec.edu.espe.crealtivastudio.model.Bill;
+import java.util.List;
+/**
+ *
+ * @author Mateo Cevallos
+ */
+public class BillController {
+//hacer clase de gestion de pagos con json xd ya hago mañana
+}
